88a5db14f2c99fde54988f5ccc9364ddd695bc04
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-10-25 01:46:21 -0200

    rss: rss config
    Setted RSS url

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,8 @@ from pelicanconf import *
 SITEURL = 'http://jvanz.github.com'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = None
 
 DELETE_OUTPUT_DIRECTORY = False
 
