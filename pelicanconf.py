e54446f1b8584860b8cb088b4a30560cf6996147
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-11-26 00:48:18 -0200

    CNAME
    Configure the CNAME with custom domain

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,8 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-STATIC_PATH = ['images']
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 # Theme stuff
 THEME = "pelican-hyde"
